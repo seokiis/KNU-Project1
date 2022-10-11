ceff0d5e79febd53d328b41dee3f3d066b8d967d
seokiis <cat1181123@naver.com>
2022-10-11 20:40:45 +0900

    body

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    font-size: 50px;
+`;
+
+function App() {
+    return <Div className="App">nothing</Div>;
+}
+
+export default App;
