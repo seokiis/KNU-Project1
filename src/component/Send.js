9bf622489639dd1e0db837cfa40de515ded4205d
bokyung <qkrqhru@naver.com>
2022-11-26 19:51:19 +0900

    [FEAT]Plot보여주기, csv파일 보여주기

---
+++ src/component/Send.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  color: white;
+  font-size: 2em;
+`;
+
+function Send(props) {
+  return <Div>"https://melonkube.com"로 결과를 보냈습니다!</Div>;
+}
+
+export default Send;
